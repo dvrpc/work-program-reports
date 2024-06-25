2f7fe3fc0698b5733eedaa931582a272b3429d20
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2024-06-25 18:03:07 -0400

    load project data dynamically

--- src/pages/amendment/{amendments.amendmentid}.tsx
+++ src/pages/amendment/{amendments.amendmentid}.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { PageProps, graphql } from "gatsby";
 import Project from "@components/Project";
 import type { Amendment as AmendmentType } from "src/Project";
+import refreshIcon from "@images/refresh.svg";
 import downloadIcon from "@images/download.svg";
 
 export const AmendmentPage: React.FC<PageProps<Queries.AmendmentPageQuery>> = ({
@@ -9,15 +10,46 @@ export const AmendmentPage: React.FC<PageProps<Queries.AmendmentPageQuery>> = ({
   location,
 }) => {
   if (amendments === null) return null;
+  const [amendment, setAmendment] =
+    React.useState<NonNullable<Queries.AmendmentPageQuery["amendments"]>>(
+      amendments
+    );
+  const [loading, setLoading] = React.useState<boolean | null>(null);
 
-  const article = (
-    <Project projects={amendments as AmendmentType} site={site} />
-  );
+  const downloadData = async (amendmentId: number) => {
+    setLoading(true);
+    const data = await fetch(
+      `https://apps.dvrpc.org/ords/workprogram25new/workprogram/draftamendment?amendmentid=${amendmentId}`
+    );
+    const json = await data.json();
+    setAmendment(json.items[0]);
+    setLoading(false);
+  };
+  const article = <Project projects={amendment as AmendmentType} site={site} />;
+  if (loading === null) downloadData(amendments.amendmentid!);
+  React.useEffect(() => setLoading(false), []);
 
   return (
     <div className="bg-gray-100 flex flex-col items-center print:bg-transparent">
       <div className="relative">
-        <div className="print:hidden max-w-[950px] w-full fixed bottom-12 flex justify-end">
+        <div className="print:hidden max-w-[950px] w-full fixed bottom-12 flex flex-col gap-8 items-end">
+          <button
+            className="block p-0 m-0 rounded-full h-16 w-16 text-center text-white bg-gray-400 hover:bg-gray-500 shadow-material"
+            onClick={() => downloadData(amendments.amendmentid!)}
+          >
+            <img
+              src={refreshIcon}
+              alt="Refresh"
+              width="32"
+              height="32"
+              className={
+                loading
+                  ? "inline align-middle animate-spin"
+                  : "inline align-middle"
+              }
+              title="Refresh Content"
+            />
+          </button>
           <a
             className="block rounded-full h-16 w-16 text-center text-white bg-blue-700 hover:bg-blue-800 shadow-material"
             href={`${location.pathname.slice(0, -1)}.pdf`}
--- src/pages/project/{projects.proid}.tsx
+++ src/pages/project/{projects.proid}.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { PageProps, graphql } from "gatsby";
 import Project from "@components/Project";
 import type { Project as ProjectType } from "src/Project";
+import refreshIcon from "@images/refresh.svg";
 import downloadIcon from "@images/download.svg";
 
 export const ProjectPage: React.FC<PageProps<Queries.ProjectPageQuery>> = ({
@@ -9,13 +10,44 @@ export const ProjectPage: React.FC<PageProps<Queries.ProjectPageQuery>> = ({
   location,
 }) => {
   if (projects === null) return null;
+  const [project, setProject] = React.useState(projects);
+  const [loading, setLoading] = React.useState<boolean | null>(null);
 
-  const article = <Project projects={projects as ProjectType} site={site} />;
+  const downloadData = async (projectId: string) => {
+    setLoading(true);
+    const data = await fetch(
+      `https://apps.dvrpc.org/ords/workprogram25new/workprogram/projects?proid=${projectId}`
+    );
+    const json = await data.json();
+    setProject(json.items[0]);
+    setLoading(false);
+  };
+  if (loading === null) downloadData(projects.proid!);
+  React.useEffect(() => setLoading(false), []);
+
+  const article = <Project projects={project as ProjectType} site={site} />;
 
   return (
     <div className="bg-gray-100 flex flex-col items-center print:bg-transparent">
       <div className="relative">
-        <div className="print:hidden max-w-[950px] w-full fixed bottom-12 flex justify-end">
+        <div className="print:hidden max-w-[950px] w-full fixed bottom-12 flex flex-col gap-8 items-end">
+          <button
+            className="block p-0 m-0 rounded-full h-16 w-16 text-center text-white bg-gray-400 hover:bg-gray-500 shadow-material"
+            onClick={() => downloadData(projects.proid!)}
+          >
+            <img
+              src={refreshIcon}
+              alt="Refresh"
+              width="32"
+              height="32"
+              className={
+                loading
+                  ? "inline align-middle animate-spin"
+                  : "inline align-middle"
+              }
+              title="Refresh Content"
+            />
+          </button>
           <a
             className="block rounded-full h-16 w-16 text-center text-white bg-blue-700 hover:bg-blue-800 shadow-material"
             href={`${location.pathname.slice(0, -1)}.pdf`}
